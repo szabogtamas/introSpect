b24e676741a29dd533c7793f0a2b2a0683bbc10c
szabogtamas <szabogtamas@gmail.com>
2020-05-28 21:10:18 +0200

    Implemented black formatting

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
 import os
 from . import _utils
 
-cmdSupport =  _utils.cmdSupport
-hint =  _utils.hint
+cmdSupport = _utils.cmdSupport
+hint = _utils.hint
 
 for module in os.listdir(os.path.dirname(__file__)):
-    if module in ['__init__.py', '_utils.py'] or module[-3:] != '.py':
+    if module in ["__init__.py", "_utils.py"] or module[-3:] != ".py":
         continue
     __import__(module[:-3], globals(), locals(), level=1)
 
